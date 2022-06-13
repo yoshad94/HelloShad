82249fa72ebf4cbf19baa2d45d7185ead8a52c09
yoshad94 <sahriar.hoss@gmail.com>
2022-06-13 16:57:40 -0400

    add file helloworld

---
+++ Helloworld.py
@@ -0,0 +1,16 @@
+username = input("What is your name")
+print ("hello " + username)
+
+Password = input("Enter 4 letter password")
+print (Password)
+
+#Apple Store App 
+#Charles Proxy 
+#Error Code 
+#Leadership / Responsibilies 
+#Mostly Manual
+#Python
+  
+
+
+# SSH Key#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
